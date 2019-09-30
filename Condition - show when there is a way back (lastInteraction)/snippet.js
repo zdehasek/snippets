4f36222503e45e905e54dd688fe1e7cf93244076
David Menger <david.menger@me.com>
2019-09-30 21:08:30 +0200

    fixed back condition

--- Condition - show when there is a way back (lastInteraction)/snippet.js
+++ Condition - show when there is a way back (lastInteraction)/snippet.js
@@ -1,1 +1,5 @@
-req => req.state.lastInteraction && req.state.lastInteraction !== '/*';
+(req, res) => {
+    const { lastInteraction: l, beforeLastInteraction: b } = req.state;
+    const c = l === res.data.lastInteractionSet ? b : l;
+    return c && c !== '/*';
+};
