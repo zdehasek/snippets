263f129ed1855a26ce33e0a11515dbd4d443a380
David <david.menger@me.com>
2020-02-24 08:06:12 +0100

    Fix postback

--- Postback - load or send data using request/snippet.js
+++ Postback - load or send data using request/snippet.js
@@ -1,6 +1,6 @@
 async (req, res) => {
     try {
-        const data = request({
+        const data = await request({
             url: 'https://api.github.com/repos/wingbotai/snippets/contents',
             method: 'GET',
             json: true
